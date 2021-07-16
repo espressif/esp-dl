5240d6ec970a8ce40c577ef72e5aef43c0ec470d
Ye Hang Yang <yehangyang@espressif.com>
2021-07-16 17:17:26 +0800

    :sparkles: model convert tool
    support: Linux, Widnows and Mac

--- tool/convert.py
+++ tool/convert.py
@@ -1,8 +1,9 @@
 import argparse
-from tool import *
+from utils import Convert
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Model generator tool')
+    parser.add_argument('-t', '--target_chip', help='esp32, esp32s2, esp32s3, esp32c3')
     parser.add_argument('-i', '--input_root', help="npy files root")
     parser.add_argument('-n', '--name', help="name of generated files")
     parser.add_argument('-o', '--output_root', help="generated source files root")
@@ -12,5 +13,8 @@ if __name__ == '__main__':
         parser.print_help()
         quit()
 
-    convert = Convert(input_root=args.input_root, output_root=args.output_root, name=args.name)
+    print(f'Generating {args.output_root}/{args.name} on {args.target_chip}...', end='')
+    convert = Convert(target_chip=args.target_chip, input_root=args.input_root, output_root=args.output_root,
+                      name=args.name)
     convert()
+    print(' Finish')
