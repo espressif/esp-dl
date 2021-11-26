fd478851f1417325303c747c5c477d1bf5cf8aaf
yuanjiong <yuanjiong@espressif.com>
2021-11-26 14:53:02 +0800

    :art: update color detection APIs

--- examples/color_detect/main/app_main.cpp
+++ examples/color_detect/main/app_main.cpp
@@ -9,37 +9,56 @@ using namespace std;
 extern "C" void app_main(void)
 {
     // set color detection model.
-    ColorDetector detector({{}, {}, {}, {}}, {}, true);
+    ColorDetector detector;
+    detector.set_color_thresh_offset({2, 4, 8});
+    detector.set_detection_shape({64, 64, 3});
 
-    // set the Tensor of test image. 
+    // set the Tensor of test image.
     Tensor<uint8_t> rgby;
     rgby.set_element((uint8_t *)rgby_image).set_shape(rgby_shape).set_auto_free(false);
 
-    // get color threshold of each color block in the image. 
+    // get color threshold of each color block in the image.
     // In the test image, we divided the picture into four regions, corresponding to four colors: red, green, blue and yellow.
-    vector<uint8_t> thresh_r = detector.cal_color_thresh(rgby, {0, 0, 128, 32});
-    vector<uint8_t> thresh_g = detector.cal_color_thresh(rgby, {0, 32, 128, 64});
-    vector<uint8_t> thresh_b = detector.cal_color_thresh(rgby, {0, 64, 128, 96});
-    vector<uint8_t> thresh_y = detector.cal_color_thresh(rgby, {0, 96, 128, 128});
+    detector.register_color(rgby, {0, 0, 128, 32}, 256, "red");
+    detector.register_color(rgby, {0, 32, 128, 64}, 256, "green");
+    detector.register_color(rgby, {0, 64, 128, 96}, 256, "blue");
+    detector.register_color(rgby, {0, 96, 128, 128}, 256, "yellow");
 
-    // set color threshold and area threshold.
-    detector.color_thresh = {thresh_r, thresh_g, thresh_b, thresh_y};
-    detector.area_thresh = {100, 100, 100, 100};
+    // get the registered colors 
+    std::vector<color_info_t> registered_colors = detector.get_registered_colors();
+    printf("the information of registered colors: \n");
+    printf("name: %s, \tthresh: %d, %d, %d, %d, %d, %d\n", registered_colors[0].name.c_str(), registered_colors[0].color_thresh[0], registered_colors[0].color_thresh[1], registered_colors[0].color_thresh[2], registered_colors[0].color_thresh[3], registered_colors[0].color_thresh[4], registered_colors[0].color_thresh[5]);
+    printf("name: %s, \tthresh: %d, %d, %d, %d, %d, %d\n", registered_colors[1].name.c_str(), registered_colors[1].color_thresh[0], registered_colors[1].color_thresh[1], registered_colors[1].color_thresh[2], registered_colors[1].color_thresh[3], registered_colors[1].color_thresh[4], registered_colors[1].color_thresh[5]);
+    printf("name: %s, \tthresh: %d, %d, %d, %d, %d, %d\n", registered_colors[2].name.c_str(), registered_colors[2].color_thresh[0], registered_colors[2].color_thresh[1], registered_colors[2].color_thresh[2], registered_colors[2].color_thresh[3], registered_colors[2].color_thresh[4], registered_colors[2].color_thresh[5]);
+    printf("name: %s, \tthresh: %d, %d, %d, %d, %d, %d\n", registered_colors[3].name.c_str(), registered_colors[3].color_thresh[0], registered_colors[3].color_thresh[1], registered_colors[3].color_thresh[2], registered_colors[3].color_thresh[3], registered_colors[3].color_thresh[4], registered_colors[3].color_thresh[5]);
 
-    // detect the four color blocks in the test picture based on the threshold set above.
-    std::vector<std::vector<components_stats_t>> &results = detector.detect(rgby);
+    // set new area threshold.
+    detector.set_area_thresh({64});
+
+    // detect the four colors in the test picture based on the threshold set above.
+    std::vector<std::vector<color_detect_result_t>> &results888 = detector.detect(rgby);
+    // print the detection results.
+    detector.print_detection_results("RGB888");
+
+    // copy the RGB565 data as the input image.
+    uint16_t *rgby_565 = (uint16_t *)malloc(rgby_shape[0] * rgby_shape[1] * sizeof(uint16_t));
+    assert(rgby_565 != NULL);
+    memcpy(rgby_565, rgby_565_image, rgby_shape[0] * rgby_shape[1] * sizeof(uint16_t));
+
+    // detect the four colors in the test picture based on the threshold set above.
+    std::vector<std::vector<color_detect_result_t>> &results565 = detector.detect(rgby_565, rgby_shape);
+    // print the detection results.
+    detector.print_detection_results("RGB565");
+
+    // delete the "red" color
+    int color_num = detector.delete_color("red");
+    printf("remained colors num: %d\n", color_num);
+
+    // detect "blue" and "yellow"  
+    detector.detect(rgby, {1, 2});
 
     // print the detection results.
-    printf("\ncolor number: %d\n\n", results.size());
-    for (int i = 0; i < results.size(); ++i)
-    {
-        printf("color %d: detected box :%d\n", i, results[i].size());
-        for (int j = 0; j < results[i].size(); ++j)
-        {
-            printf("center: (%d, %d)\n", results[i][j].center[0], results[i][j].center[1]);
-            printf("box: (%d, %d), (%d, %d)\n", results[i][j].box[0], results[i][j].box[1], results[i][j].box[2], results[i][j].box[3]);
-            printf("area: %d\n", results[i][j].area);
-        }
-        printf("\n");
-    }
+    detector.print_detection_results("Blue, Yellow");
+    // print the segmentation results.
+    detector.print_segmentation_results("Blue, Yellow");
 }
