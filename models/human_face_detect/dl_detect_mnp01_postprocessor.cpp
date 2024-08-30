9b2347b519360f08eb71135471cc04a7795bf762
Fan Shen Wei <fanshenwei@espressif.com>
2024-08-30 17:58:46 +0800

    add model & examples

---
+++ models/human_face_detect/dl_detect_mnp01_postprocessor.cpp
@@ -0,0 +1,95 @@
+#include "dl_detect_mnp01_postprocessor.hpp"
+#include "dl_math.hpp"
+#include <algorithm>
+#include <cmath>
+
+namespace dl
+{
+    namespace detect
+    {
+        template <typename feature_t>
+        void MNP01Postprocessor<feature_t>::parse_stage(TensorBase *score, TensorBase *box, TensorBase *landmark, const int stage_index)
+        {
+            std::vector<std::vector<int>> &anchor_shape = this->stages[stage_index].anchor_shape;
+
+            int H = score->shape[0];
+            int W = score->shape[1];
+            int A = anchor_shape.size();
+            int C = score->shape[2] / A;
+            feature_t *score_element = (feature_t *)score->get_element_ptr();
+            feature_t *box_element = (feature_t *)box->get_element_ptr();
+            feature_t *landmark_element = (feature_t *)landmark->get_element_ptr();
+
+            for (size_t y = 0; y < H; y++) // height
+            {
+                for (size_t x = 0; x < W; x++) // width
+                {
+                    for (size_t a = 0; a < A; a++) // anchor number
+                    {
+                        // softmax
+                        float scores[C];
+                        scores[0] = score_element[0] * DL_SCALE(score->exponent);
+                        float max_score = scores[0];
+                        int max_score_c = 0;
+                        for (int i = 1; i < C; i++)
+                        {
+                            scores[i] = score_element[i] * DL_SCALE(score->exponent);
+                            if (max_score < scores[i])
+                            {
+                                max_score_c = i;
+                                max_score = scores[i];
+                            }
+                        }
+                        float sum = 0;
+                        for (int i = 0; i < C; i++)
+                        {
+                            sum += expf(scores[i] - max_score);
+                        }
+                        max_score = 1. / sum;
+
+                        if (max_score > score_threshold)
+                        {
+                            int anchor_h = anchor_shape[a][0];
+                            int anchor_w = anchor_shape[a][1];
+                            result_t new_box = {
+                                max_score_c,
+                                max_score,
+                                {(int)(anchor_w * box_element[0] * DL_SCALE(box->exponent) / this->resize_scale_x + this->top_left_x),
+                                 (int)(anchor_h * box_element[1] * DL_SCALE(box->exponent) / this->resize_scale_y + this->top_left_y),
+                                 (int)((anchor_w * box_element[2] * DL_SCALE(box->exponent) + anchor_w) / this->resize_scale_x + this->top_left_x),
+                                 (int)((anchor_h * box_element[3] * DL_SCALE(box->exponent) + anchor_h) / this->resize_scale_y + this->top_left_y)},
+                                {(int)(anchor_w * landmark_element[0] * DL_SCALE(landmark->exponent) / this->resize_scale_x + this->top_left_x),
+                                 (int)(anchor_h * landmark_element[1] * DL_SCALE(landmark->exponent) / this->resize_scale_y + this->top_left_y),
+                                 (int)(anchor_w * landmark_element[2] * DL_SCALE(landmark->exponent) / this->resize_scale_x + this->top_left_x),
+                                 (int)(anchor_h * landmark_element[3] * DL_SCALE(landmark->exponent) / this->resize_scale_y + this->top_left_y),
+                                 (int)(anchor_w * landmark_element[4] * DL_SCALE(landmark->exponent) / this->resize_scale_x + this->top_left_x),
+                                 (int)(anchor_h * landmark_element[5] * DL_SCALE(landmark->exponent) / this->resize_scale_y + this->top_left_y),
+                                 (int)(anchor_w * landmark_element[6] * DL_SCALE(landmark->exponent) / this->resize_scale_x + this->top_left_x),
+                                 (int)(anchor_h * landmark_element[7] * DL_SCALE(landmark->exponent) / this->resize_scale_y + this->top_left_y),
+                                 (int)(anchor_w * landmark_element[8] * DL_SCALE(landmark->exponent) / this->resize_scale_x + this->top_left_x),
+                                 (int)(anchor_h * landmark_element[9] * DL_SCALE(landmark->exponent) / this->resize_scale_y + this->top_left_y),
+                                }};
+
+                            this->box_list.insert(std::upper_bound(this->box_list.begin(), this->box_list.end(), new_box, compare_greater_box), new_box);
+                        }
+                        score_element += C;
+                        box_element += 4;
+                        landmark_element += 10;
+                    }
+                }
+            }
+        }
+
+        template <typename feature_t>
+        void MNP01Postprocessor<feature_t>::postprocess(std::map<std::string, TensorBase *>& model_outputs_map)
+        {
+            TensorBase *score = model_outputs_map.at("score");
+            TensorBase *bbox = model_outputs_map.at("box");
+            TensorBase *landmark = model_outputs_map.at("landmark");
+            this->parse_stage(score, bbox, landmark, 0);
+        }
+
+        template class MNP01Postprocessor<int8_t>;
+        template class MNP01Postprocessor<int16_t>;
+    }
+}
---
+++ models/pedestrian_detect/dl_detect_pedestrian_postprocessor.cpp
@@ -0,0 +1,88 @@
+#include "dl_detect_pedestrian_postprocessor.hpp"
+#include "dl_math.hpp"
+#include <algorithm>
+#include <cmath>
+
+namespace dl
+{
+    namespace detect
+    {
+        template <typename feature_t>
+        void PedestrianPostprocessor<feature_t>::parse_stage(TensorBase *score, TensorBase *box, const int stage_index)
+        {
+            int stride_y = this->stages[stage_index].stride_y;
+            int stride_x = this->stages[stage_index].stride_x;
+
+            int offset_y = this->stages[stage_index].offset_y;
+            int offset_x = this->stages[stage_index].offset_x;
+
+            int H = score->shape[0];
+            int W = score->shape[1];
+            int C = score->shape[2];
+            feature_t *score_element = (feature_t *)score->get_element_ptr();
+            feature_t score_threshold_quant;
+            if(std::is_same<feature_t, int8_t>::value)
+            {
+                score_threshold_quant = (feature_t) DL_CLIP(dl_esp32p4_round_half_even(this->score_threshold * this->score_threshold / DL_SCALE(score->exponent)), -128, 127);
+            }
+            else{
+                score_threshold_quant = (feature_t) DL_CLIP(dl_esp32p4_round_half_even(this->score_threshold * this->score_threshold / DL_SCALE(score->exponent)), -32768, 32767);
+            }
+
+            feature_t *box_element = (feature_t *)box->get_element_ptr();
+
+            for (size_t y = 0; y < H; y++) // height
+            {
+                for (size_t x = 0; x < W; x++) // width
+                {
+                    for (size_t c = 0; c < C; c++) // category number
+                    {
+                        if (*score_element > score_threshold_quant)
+                        {
+                            int center_y = y * stride_y + offset_y;
+                            int center_x = x * stride_x + offset_x;
+
+                            float box_element_f[32];
+                            for (int i = 0; i < 32; i++)
+                            {
+                                box_element_f[i] = box_element[i] * DL_SCALE(box->exponent);
+                            }
+
+                            result_t new_box = {
+                                (int) c,
+                                (float)sqrt(*score_element * DL_SCALE(score->exponent)),
+                                {(int)((center_x - dl::math::dfl_integral(box_element_f, 7) * stride_x) / this->resize_scale_x),
+                                (int)((center_y - dl::math::dfl_integral(box_element_f + 8, 7) * stride_y) / this->resize_scale_y),
+                                (int)((center_x + dl::math::dfl_integral(box_element_f + 16, 7) * stride_x) / this->resize_scale_x),
+                                (int)((center_y + dl::math::dfl_integral(box_element_f + 24, 7) * stride_y) / this->resize_scale_y)},
+                                {0}};
+                            
+                            this->box_list.insert(std::upper_bound(this->box_list.begin(), this->box_list.end(), new_box, compare_greater_box), new_box);
+                        }
+                    }
+                    score_element++;
+                    box_element += 32;
+                }
+            }
+        }
+
+        template <typename feature_t>
+        void PedestrianPostprocessor<feature_t>::postprocess(std::map<std::string, TensorBase *>& model_outputs_map)
+        {
+            TensorBase *score0 = model_outputs_map.at("score0");
+            TensorBase *bbox0 = model_outputs_map.at("bbox0");
+            TensorBase *score1 = model_outputs_map.at("score1");
+            TensorBase *bbox1 = model_outputs_map.at("bbox1");
+            TensorBase *score2 = model_outputs_map.at("score2");
+            TensorBase *bbox2 = model_outputs_map.at("bbox2");
+            
+            this->parse_stage(score0, bbox0, 0);
+            this->parse_stage(score1, bbox1, 1);
+            this->parse_stage(score2, bbox2, 2);
+            this->nms();
+        }
+
+        template class PedestrianPostprocessor<int8_t>;
+        template class PedestrianPostprocessor<int16_t>;
+    }
+}
