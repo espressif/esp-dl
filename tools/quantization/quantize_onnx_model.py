554e80d2cf5f45bae80682ba3ce5355140a5aca1
Fan Shen Wei <fanshenwei@espressif.com>
2024-12-05 14:51:35 +0800

    fix: fix quant tool script issue in windows.

--- tools/quantization/quantize_onnx_model.py
+++ tools/quantization/quantize_onnx_model.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-from typing import Iterable, Tuple
+from typing import Iterable, Tuple, List, Tuple
 
 import torch
 from datasets.imagenet_util import (
@@ -10,12 +10,17 @@ from datasets.imagenet_util import (
 from ppq import QuantizationSettingFactory, QuantizationSetting
 from ppq.api import espdl_quantize_onnx, get_target_platform
 from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data.dataset import Subset
+import urllib.request
+import zipfile
 
 
 def quant_setting_mobilenet_v2(
     onnx_path: str,
-    optim_quant_method: list[str] = None,
-) -> tuple[QuantizationSetting, str]:
+    optim_quant_method: List[str] = None,
+) -> Tuple[QuantizationSetting, str]:
     """Quantize onnx model with optim_quant_method.
 
     Args:
@@ -60,91 +65,119 @@ def quant_setting_mobilenet_v2(
     return quant_setting, onnx_path
 
 
-BATCH_SIZE = 32
-INPUT_SHAPE = [3, 224, 224]
-DEVICE = (
-    "cpu"  #  'cuda' or 'cpu', if you use cuda, please make sure that cuda is available
-)
-TARGET = "esp32p4"
-NUM_OF_BITS = 8
-ONNX_PATH = "./models/torch/mobilenet_v2.onnx"  #'models/onnx/mobilenet_v2.onnx'
-ESPDL_MODLE_PATH = "models/onnx/mobilenet_v2.espdl"
-CALIB_DIR = "./imagenet"
-
-# Download mobilenet_v2 model from onnx models and dataset
-imagenet_url = "https://dl.espressif.com/public/imagenet_calib.zip"
-os.makedirs(CALIB_DIR, exist_ok=True)
-subprocess.run(["wget", imagenet_url])
-subprocess.run(["unzip", "imagenet_calib.zip", "-d", CALIB_DIR])
-CALIB_DIR = os.path.join(CALIB_DIR, "calib")
-
-# -------------------------------------------
-# Prepare Calibration Dataset
-# --------------------------------------------
-if os.path.exists(CALIB_DIR):
-    print(f"load imagenet calibration dataset from directory: {CALIB_DIR}")
-    dataloader = load_imagenet_from_directory(
-        directory=CALIB_DIR,
-        batchsize=BATCH_SIZE,
-        shuffle=False,
-        subset=1024,
-        require_label=False,
-        num_of_workers=4,
-    )
-else:
-    # Random calibration dataset only for debug
-    print("load random calibration dataset")
-
-    def load_random_calibration_dataset() -> Iterable:
-        return [torch.rand(size=INPUT_SHAPE) for _ in range(BATCH_SIZE)]
-
-    # Load training data for creating a calibration dataloader.
-    dataloader = DataLoader(
-        dataset=load_random_calibration_dataset(), batch_size=BATCH_SIZE, shuffle=False
-    )
+def collate_fn1(x: Tuple) -> torch.Tensor:
+    return torch.cat([sample[0].unsqueeze(0) for sample in x], dim=0)
 
 
-def collate_fn(batch: torch.Tensor) -> torch.Tensor:
+def collate_fn2(batch: torch.Tensor) -> torch.Tensor:
     return batch.to(DEVICE)
 
 
-# -------------------------------------------
-# Quantize ONNX Model.
-# --------------------------------------------
-
-# create a setting for quantizing your network with ESPDL.
-# if you don't need to optimize quantization, set the input 1 of the quant_setting_mobilenet_v2 function None
-# Example: Using LayerwiseEqualization_quantization
-quant_setting, ONNX_PATH = quant_setting_mobilenet_v2(
-    ONNX_PATH, ["LayerwiseEqualization_quantization"]
-)
+def report_hook(blocknum, blocksize, total):
+    downloaded = blocknum * blocksize
+    percent = downloaded / total * 100
+    print(f"\rDownloading calibration dataset: {percent:.2f}%", end="")
 
 
-quant_ppq_graph = espdl_quantize_onnx(
-    onnx_import_file=ONNX_PATH,
-    espdl_export_file=ESPDL_MODLE_PATH,
-    calib_dataloader=dataloader,
-    calib_steps=32,
-    input_shape=[1] + INPUT_SHAPE,
-    target=TARGET,
-    num_of_bits=NUM_OF_BITS,
-    collate_fn=collate_fn,
-    setting=quant_setting,
-    device=DEVICE,
-    error_report=True,
-    skip_export=False,
-    export_test_values=False,
-    verbose=1,
-)
+if __name__ == "__main__":
+    BATCH_SIZE = 32
+    INPUT_SHAPE = [3, 224, 224]
+    DEVICE = "cpu"  #  'cuda' or 'cpu', if you use cuda, please make sure that cuda is available
+    TARGET = "esp32p4"
+    NUM_OF_BITS = 8
+    ONNX_PATH = "./models/torch/mobilenet_v2.onnx"  #'models/onnx/mobilenet_v2.onnx'
+    ESPDL_MODLE_PATH = "models/onnx/mobilenet_v2.espdl"
+    CALIB_DIR = "./imagenet"
 
-# -------------------------------------------
-# Evaluate Quantized Model.
-# --------------------------------------------
+    # Download mobilenet_v2 model from onnx models and dataset
+    imagenet_url = "https://dl.espressif.com/public/imagenet_calib.zip"
+    os.makedirs(CALIB_DIR, exist_ok=True)
+    if not os.path.exists("imagenet_calib.zip"):
+        urllib.request.urlretrieve(
+            imagenet_url, "imagenet_calib.zip", reporthook=report_hook
+        )
+    if not os.path.exists(os.path.join(CALIB_DIR, "calib")):
+        with zipfile.ZipFile("imagenet_calib.zip", "r") as zip_file:
+            zip_file.extractall(CALIB_DIR)
+    CALIB_DIR = os.path.join(CALIB_DIR, "calib")
 
-evaluate_ppq_module_with_imagenet(
-    model=quant_ppq_graph,
-    imagenet_validation_dir=CALIB_DIR,
-    batchsize=BATCH_SIZE,
-    device=DEVICE,
-    verbose=1,
-)
+    # -------------------------------------------
+    # Prepare Calibration Dataset
+    # --------------------------------------------
+    if os.path.exists(CALIB_DIR):
+        print(f"load imagenet calibration dataset from directory: {CALIB_DIR}")
+        dataset = datasets.ImageFolder(
+            CALIB_DIR,
+            transforms.Compose(
+                [
+                    transforms.Resize(256),
+                    transforms.CenterCrop(224),
+                    transforms.ToTensor(),
+                    transforms.Normalize(
+                        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                    ),
+                ]
+            ),
+        )
+        dataset = Subset(dataset, indices=[_ for _ in range(0, 1024)])
+        dataloader = DataLoader(
+            dataset=dataset,
+            batch_size=BATCH_SIZE,
+            shuffle=False,
+            num_workers=4,
+            pin_memory=False,
+            collate_fn=collate_fn1,
+        )
+    else:
+        # Random calibration dataset only for debug
+        print("load random calibration dataset")
+
+        def load_random_calibration_dataset() -> Iterable:
+            return [torch.rand(size=INPUT_SHAPE) for _ in range(BATCH_SIZE)]
+
+        # Load training data for creating a calibration dataloader.
+        dataloader = DataLoader(
+            dataset=load_random_calibration_dataset(),
+            batch_size=BATCH_SIZE,
+            shuffle=False,
+        )
+
+    # -------------------------------------------
+    # Quantize ONNX Model.
+    # --------------------------------------------
+
+    # create a setting for quantizing your network with ESPDL.
+    # if you don't need to optimize quantization, set the input 1 of the quant_setting_mobilenet_v2 function None
+    # Example: Using LayerwiseEqualization_quantization
+    quant_setting, ONNX_PATH = quant_setting_mobilenet_v2(
+        ONNX_PATH, ["LayerwiseEqualization_quantization"]
+    )
+
+    quant_ppq_graph = espdl_quantize_onnx(
+        onnx_import_file=ONNX_PATH,
+        espdl_export_file=ESPDL_MODLE_PATH,
+        calib_dataloader=dataloader,
+        calib_steps=32,
+        input_shape=[1] + INPUT_SHAPE,
+        target=TARGET,
+        num_of_bits=NUM_OF_BITS,
+        collate_fn=collate_fn2,
+        setting=quant_setting,
+        device=DEVICE,
+        error_report=True,
+        skip_export=False,
+        export_test_values=False,
+        verbose=1,
+    )
+
+    # -------------------------------------------
+    # Evaluate Quantized Model.
+    # --------------------------------------------
+
+    evaluate_ppq_module_with_imagenet(
+        model=quant_ppq_graph,
+        imagenet_validation_dir=CALIB_DIR,
+        batchsize=BATCH_SIZE,
+        device=DEVICE,
+        verbose=1,
+    )
