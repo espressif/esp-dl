8d93ab847b33166ce7e7ba552dba43d4ebdadad7
zhuwenjing <zhuwenjing@espressif.com>
2025-03-28 10:51:50 +0800

    modify Attention class while exporting onnx

--- models/coco_detect/models/export_onnx.py
+++ models/coco_detect/models/export_onnx.py
@@ -1,5 +1,7 @@
 from ultralytics import YOLO
-from ultralytics.nn.modules import Detect
+from ultralytics.nn.modules import Detect, Attention
+
+import torch
 
 
 class ESP_Detect(Detect):
@@ -18,9 +20,38 @@ class ESP_Detect(Detect):
         return box0, score0, box1, score1, box2, score2
 
 
+class ESP_Attention(Attention):
+    def forward(self, x):
+        """
+        Forward pass of the Attention module.
+
+        Args:
+            x (torch.Tensor): The input tensor.
+
+        Returns:
+            (torch.Tensor): The output tensor after self-attention.
+        """
+        B, C, H, W = x.shape
+        N = H * W
+        qkv = self.qkv(x)
+        q, k, v = qkv.view(
+            -1, self.num_heads, self.key_dim * 2 + self.head_dim, N
+        ).split([self.key_dim, self.key_dim, self.head_dim], dim=2)
+        attn = (q.transpose(-2, -1) @ k) * self.scale
+        attn = attn.softmax(dim=-1)
+        x = (v @ attn.transpose(-2, -1)).view(-1, C, H, W) + self.pe(
+            v.reshape(-1, C, H, W)
+        )
+        x = self.proj(x)
+        return x
+
+
 model = YOLO("yolo11n.pt")
 for m in model.modules():
+    if isinstance(m, Attention):
+        m.forward = ESP_Attention.forward.__get__(m)
     if isinstance(m, Detect):
         m.forward = ESP_Detect.forward.__get__(m)
+
 
 model.export(format="onnx", simplify=True, opset=13, imgsz=640)
