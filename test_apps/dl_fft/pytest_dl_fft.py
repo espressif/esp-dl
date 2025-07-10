0ec9c2ad4b39fa3d3bd5a7d095029daa9585a624
xysun <sunxiangyu@espressif.com>
2025-07-10 14:14:34 +0800

    update esp-dl version

--- test_apps/dl_fft/pytest_dl_fft.py
+++ test_apps/dl_fft/pytest_dl_fft.py
@@ -4,8 +4,10 @@ from pytest_embedded import Dut
 
 @pytest.mark.target("esp32p4")
 @pytest.mark.target("esp32s3")
+@pytest.mark.target("esp32")
 @pytest.mark.env("esp32p4")
 @pytest.mark.env("esp32s3")
+@pytest.mark.env("esp32")
 def test_model_common(dut: Dut) -> None:
     dut.expect_exact("Press ENTER to see the list of tests.")
     dut.write("[dl_fft]")
