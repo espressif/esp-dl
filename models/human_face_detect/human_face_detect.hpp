bca172cf9405fee5e06adaa71f8337b3cad09226
zhuwenjing <zhuwenjing@espressif.com>
2026-03-06 19:54:42 +0800

    support espdet_pico human face detect

--- models/human_face_detect/human_face_detect.hpp
+++ models/human_face_detect/human_face_detect.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
 #include "dl_detect_base.hpp"
+#include "dl_detect_espdet_postprocessor.hpp"
 #include "dl_detect_mnp_postprocessor.hpp"
 #include "dl_detect_msr_postprocessor.hpp"
+
 namespace human_face_detect {
 class MSR : public dl::detect::DetectImpl {
 public:
@@ -50,11 +52,22 @@ public:
     dl::Model *get_raw_model(int idx) override;
 };
 
+class ESPDet : public dl::detect::DetectImpl {
+public:
+    static inline constexpr float default_score_thr = 0.5;
+    static inline constexpr float default_nms_thr = 0.7;
+    ESPDet(const char *model_name, float score_thr, float nms_thr);
+};
+
 } // namespace human_face_detect
 
 class HumanFaceDetect : public dl::detect::DetectWrapper {
 public:
-    typedef enum { MSRMNP_S8_V1 } model_type_t;
+    typedef enum {
+        MSRMNP_S8_V1,
+        ESPDET_PICO_224_224_FACE,
+        ESPDET_PICO_416_416_FACE,
+    } model_type_t;
 
     HumanFaceDetect(model_type_t model_type = static_cast<model_type_t>(CONFIG_DEFAULT_HUMAN_FACE_DETECT_MODEL),
                     bool lazy_load = true);
