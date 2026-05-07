d3b71c1aba724fd0935b92aa759b6dd3178570b1
sunxiangyu <sunxiangyu@espressif.com>
2026-05-07 10:23:57 +0800

    feat: add esp32p4 int16 fft

--- tools/dl_fft/base/isa/dl_fft_platform.h
+++ tools/dl_fft/base/isa/dl_fft_platform.h
@@ -32,6 +32,13 @@ void dl_fft2r_fc32_arp4_(float *data, int N, float *table);
 void dl_ifft2r_fc32_arp4_(float *data, int N, float *table);
 void dl_fft4r_fc32_arp4_(float *data, int N, float *table, int table_size);
 void dl_ifft4r_fc32_arp4_(float *data, int N, float *table, int table_size);
+int16_t dl_fft_reduce_max_arp4_(const int16_t *inputs, int len);
+int16_t dl_reduce_abs_max_arp4_(const int16_t *inputs, int len);
+void dl_bitrev2r_sc16_arp4_(int16_t *data, int cpx_points, int log2N);
+void dl_fft2r_sc16_dif_arp4_(int16_t *data, int16_t *table, int shift, int num_stages, int cpx_points);
+void dl_ifft2r_sc16_dif_arp4_(int16_t *data, int16_t *table, int shift, int num_stages, int cpx_points);
+void dl_fft2r_sc16_dif_hp_arp4_(int16_t *data, int16_t *table, int num_stages, int cpx_points, int *out_shift);
+void dl_ifft2r_sc16_dif_hp_arp4_(int16_t *data, int16_t *table, int num_stages, int cpx_points, int *out_shift);
 #endif
 
 #ifdef __cplusplus
