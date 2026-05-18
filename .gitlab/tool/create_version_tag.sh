d2a7767be1edcdbd8d2e9657d252cc127f124f41
sunxiangyu <sunxiangyu@espressif.com>
2026-05-18 11:40:57 +0800

    ci: release version tag by ci

--- .gitlab/tool/create_version_tag.sh
+++ .gitlab/tool/create_version_tag.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 if git ls-remote --tags origin "refs/tags/v${VERSION}" | grep -q "refs/tags/v${VERSION}"; then
     echo "Tag v${VERSION} already exists, skipping..."
 else
@@ -8,15 +10,15 @@ else
     git tag -a "v${VERSION}" -m "Release version v${VERSION}"
     git push origin "v${VERSION}"
 
-    # Trigger a new pipeline for the newly created tag
-    # This is needed because tags created by CI jobs don't automatically trigger pipelines
-    if [ -n "${CI_TRIGGER_TOKEN}" ]; then
-        echo "Triggering pipeline for tag v${VERSION}..."
-        curl -X POST \
-            -F token="${CI_TRIGGER_TOKEN}" \
-            -F ref="v${VERSION}" \
-            "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/trigger/pipeline"
+    # Also push the new tag to GitHub mirror.
+    # The master/release branch push pipeline has already run the full CI for
+    # this commit, so we deliberately do NOT trigger a new tag pipeline here.
+    if [ -n "${GITHUB_PUSH_TOKEN}" ]; then
+        echo "Pushing tag v${VERSION} to GitHub..."
+        git remote remove github &>/dev/null || true
+        git remote add github "https://${GITHUB_PUSH_TOKEN}@github.com/espressif/esp-dl.git"
+        git push github "v${VERSION}"
     else
-        echo "Warning: CI_TRIGGER_TOKEN not set, pipeline for tag v${VERSION} needs to be triggered manually"
+        echo "Warning: GITHUB_PUSH_TOKEN not set, tag v${VERSION} was not pushed to GitHub"
     fi
 fi
