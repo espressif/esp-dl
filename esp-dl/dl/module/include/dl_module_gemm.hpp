6f30c8b1724118dda407364df1c9688d849325d6
sunxiangyu <sunxiangyu@espressif.com>
2026-08-24 20:06:24 +0800

    feat: support w8a16 gemm

--- esp-dl/dl/module/include/dl_module_gemm.hpp
+++ esp-dl/dl/module/include/dl_module_gemm.hpp
@@ -3,6 +3,7 @@
 #include "dl_base_conv2d.hpp"
 #include "dl_base_depthwise_conv2d.hpp"
 #include "dl_module_base.hpp"
+#include <string>
 #include <typeinfo>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -79,10 +80,12 @@ public:
             base::conv2d<int8_t, int32_t, int32_t>(args);
         } else if (quant_type == QUANT_TYPE_SYMM_16BIT) {
             base::conv2d<int16_t, int32_t, int64_t>(args);
+        } else if (quant_type == QUANT_TYPE_SYMM_W8A16) {
+            base::conv2d<int16_t, int32_t, int64_t, int8_t>(args);
         }
     }
 
-    template <typename T>
+    template <typename T, typename filter_t = T>
     void forward_template(ModelContext *context, runtime_mode_t mode)
     {
         std::vector<int> padding(4, 0);
@@ -98,17 +101,17 @@ public:
         input0->set_shape({1, 1, input0->get_size() / origin_input_shape.back(), origin_input_shape.back()});
         output->set_shape({1, 1, output->get_size() / origin_output_shape.back(), origin_output_shape.back()});
 
-        base::ConvOpArgs<T> m_args(output,
-                                   input0,
-                                   padding,
-                                   filter,
-                                   {1, 1} /*strides*/,
-                                   {1, 1} /*dilations*/,
-                                   1 /*group*/,
-                                   bias,
-                                   this->activation,
-                                   nullptr,
-                                   mode); // do not support PReLU and Leaky RelU
+        base::ConvOpArgs<T, filter_t> m_args(output,
+                                             input0,
+                                             padding,
+                                             filter,
+                                             {1, 1} /*strides*/,
+                                             {1, 1} /*dilations*/,
+                                             1 /*group*/,
+                                             bias,
+                                             this->activation,
+                                             nullptr,
+                                             mode); // do not support PReLU and Leaky RelU
         int task_size = m_args.size();
         if (task_size == 1) { // single task
             forward_args((void *)&m_args.get_args(0));
@@ -130,6 +133,8 @@ public:
             forward_template<int8_t>(context, mode);
         } else if (quant_type == QUANT_TYPE_SYMM_16BIT) {
             forward_template<int16_t>(context, mode);
+        } else if (quant_type == QUANT_TYPE_SYMM_W8A16) {
+            forward_template<int16_t, int8_t>(context, mode);
         }
     }
 
@@ -141,17 +146,26 @@ public:
         Module *gemm_op = nullptr;
 
         activation_type_t activation_type;
-        quant_type_t quant_type;
+        quant_type_t quant_type = QUANT_TYPE_NONE;
+        std::string quant_type_str;
         int transA = -1, transB = -1;
         fbs_model->get_operation_attribute(node_name, "activation", activation_type);
-        fbs_model->get_operation_attribute(node_name, "quant_type", quant_type);
+        // Prebuilt fbs_model only maps S8/S16/F32. Read the raw string first so
+        // W8A16 is not lost if the library treats it as unknown.
+        if (fbs_model->get_operation_attribute(node_name, "quant_type", quant_type_str) == ESP_OK &&
+            quant_type_str == "W8A16") {
+            quant_type = QUANT_TYPE_SYMM_W8A16;
+        } else {
+            fbs_model->get_operation_attribute(node_name, "quant_type", quant_type);
+        }
         fbs_model->get_operation_attribute(node_name, "transA", transA);
         fbs_model->get_operation_attribute(node_name, "transB", transB);
         assert(transA == -1 || transA == 0);
         assert(transB == -1 || transB == 0);
 
         // Create module
-        if (quant_type == QUANT_TYPE_SYMM_8BIT || quant_type == QUANT_TYPE_SYMM_16BIT) {
+        if (quant_type == QUANT_TYPE_SYMM_8BIT || quant_type == QUANT_TYPE_SYMM_16BIT ||
+            quant_type == QUANT_TYPE_SYMM_W8A16) {
             gemm_op = new Gemm(activation_type, node_name.c_str(), quant_type);
         }
 
